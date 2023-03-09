7d30f35978e44466b99dd522b2d8709eeede7101
ZaYn <105445502+ZeYn-Galaxy@users.noreply.github.com>
2023-03-09 22:17:49 +0800

    StarterPlayerScripts

---
+++ Client.lua
@@ -0,0 +1,37 @@
+
+local DefaultParticle = {}
+
+for i,v in pairs(workspace:GetDescendants()) do
+	if v:IsA("ParticleEmitter") then
+		local EmitterRealRate = v.Rate
+		table.insert(DefaultParticle, {
+			Particle = v,
+			Default = v.Rate
+		})
+	end
+end
+
+function getParticle(particle)
+	for i,v in pairs(DefaultParticle) do
+		if v.Particle == particle then
+			return i
+		end
+	end
+	return false
+end
+
+
+function ParticleGraphics()
+	for i,v in pairs(workspace:GetDescendants()) do
+		if v:IsA("ParticleEmitter") then
+			local GraphicsLevel = UserSettings().GameSettings.SavedQualityLevel.Value
+			local index = getParticle(v)
+			if index then
+				local Emitter = DefaultParticle[index]
+				v.Rate = Emitter.Default * 10 / GraphicsLevel
+			end
+		end
+	end
+end
+
+UserSettings().GameSettings:GetPropertyChangedSignal("SavedQualityLevel"):Connect(ParticleGraphics)
